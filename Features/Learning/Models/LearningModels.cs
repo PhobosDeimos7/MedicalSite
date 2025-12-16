7becca68f686cb30c2b73edaaa2b6add2964fc90
PhobosDeimos7 <sanjeev.raman.iyer@gmail.com>
2025-12-16 22:30:30 +0530

    Added SubjectCard.razor for differentiating clinical and para-clinical. Made changes to Flashcard, learningmodels, learningcentre to create contrast.

--- Features/Learning/Models/LearningModels.cs
+++ Features/Learning/Models/LearningModels.cs
@@ -10,6 +10,7 @@ public class Subject
     public string Id { get; set; } = "";
     public string Name { get; set; } = "";
     public string Description { get; set; } = "";
+    public string Category { get; set; } = "Para-clinical"; 
     public List<Topic> Topics { get; set; } = new();
 }
 
